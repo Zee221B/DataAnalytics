75895c70f1e2b363aa3a22adbc05231c5b9a648c
Zarreena <zarreenabrown@gmail.com>
2023-09-19 12:56:49 +0100

    changes to task 1

--- database_utils.py
+++ database_utils.py
@@ -2,9 +2,8 @@ import yaml
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy import inspect
-from data_extraction import DataExtractor
-from data_cleaning import DataCleaning
+ # from data_extraction import DataExtractor
+ # from data_cleaning import DataCleaning
 
-
-class DatabaseConnector:
-    
+class DataBaseConnector:
+  
